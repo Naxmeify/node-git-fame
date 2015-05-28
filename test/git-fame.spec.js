2202637f2c1919a5c4bbc3e111ec665a00750802
MRW Neundorf <matt@nax.me>
2015-05-28 14:40:06 +0200

    added dummy test struc for lib

---
+++ test/git-fame.spec.js
@@ -0,0 +1,15 @@
+describe('git-fame', function () {
+
+  context('method isGitRepo()', function() {
+
+    it('should return true for current directory', function(done) {
+        done();
+    });
+
+    it('should return false for a repoless path', function(done) {
+        done();
+    });
+
+  });
+
+});
